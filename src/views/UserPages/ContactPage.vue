438de2e9fb163e5bbf3d40cc37a79f1b56fce505
verikoo <veriko.lomadze1@gmail.com>
2021-09-09 15:55:15 +0400

    form-input new

--- src/views/UserPages/ContactPage.vue
+++ src/views/UserPages/ContactPage.vue
@@ -30,8 +30,49 @@
 
 
    
-   <div class="form">
+   <div class="form" style="border: 2px solid green">
+   <div class="formTitle" style="border:2px solid red" >
+     <h2>გამოაგზავნეთ შეკითხვა ფორმის მეშვეობით </h2>
+   </div>
+   <div class="login-box">
+  <form>
+    <div class="user-box" >
+      <input type="text" name="" required="">
+      <label>სახელი</label>
+    </div>
+    <div class="user-box" >
+      <input type="text" name="" required="">
+      <label>გვარი</label>
+    </div>
+    <div class="user-box">
+      <input type="email" name="" required="">
+      <label>ფოსტა</label>
+    </div>
+     <div class="user-box">
+      <input type="number" name="" required="">
+      <label>საკონტაქტო ნომერი</label>
+    </div>
+    <div class="user-box">
+      <select name="category" id="category">
+        <option value="kategory1 "> თემა: პარტრნიორობა</option>
+        <option value="kategory1 "> თემა: პარტრნიორობა</option>
+        <option value="kategory1 "> თემა: პარტრნიორობა</option>
+        <option value="kategory1 "> თემა: პარტრნიორობა</option>
+      </select>
+      <!-- <label for="category">airchiet tematika:</label> -->
 
+    
+    </div>
+  
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+</div>
    </div>
 
   </div>
@@ -56,9 +97,12 @@
   align-items: center;
 }
 .contactUsTitle h2 {
-  margin-top: 50px;
+  margin-top: 4vw;
   /* border: 2px solid red; */
   margin-left: 10px;
+}
+.socialIconeInside h3{
+  font-size: 1vw;
 }
 .contactUsTitle img {
   /* border: 2px solid red; */
@@ -90,6 +134,186 @@
 /* social icones */
 
 /* contact-form */
+.form{
+  padding: 60px;
+}
+.login-box {
+  /* position: absolute; */
+
+  justify-content: center !important;
+   position: relative;
+  margin:auto  !important;
+  width: 40%;
+  padding: 40px;
+  margin-top: 40px !important;
+  background: #591a92;
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px #591a92;
+  border-radius: 10px;
+  border: 2px solid rgb(5, 233, 92);
+}
+
+.login-box h2 {
+  margin: 0 0 30px;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+}
+
+.login-box .user-box {
+  position: relative;
+/* border:2px solid yellow; */
+}
+
+.login-box .user-box input, select {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+
+
+
+.login-box .user-box label {
+  position: absolute;
+  top:0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: .5s;
+}
+
+.login-box .user-box input:focus ~ label,
+.login-box .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #AB4DF7;
+  font-size: 12px;
+}
+
+
+
+.login-box form a {
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #AB4DF7;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: .5s;
+  margin-top: 40px;
+  letter-spacing: 4px
+}
+
+.login-box a:hover, select{
+  background: #AB4DF7;
+  color: rgb(255, 255, 255);
+  border-radius: 1px;
+  box-shadow: 0 0 5px #AB4DF7,
+              0 0 5px #AB4DF7,
+              0 0 10px #AB4DF7,
+              0 0 50px #AB4DF7;
+}
+
+
+.login-box a span {
+  position: absolute;
+  display: block;
+}
+
+.login-box a span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #AB4DF7);
+  animation: btn-anim1 1s linear infinite;
+}
+
+@keyframes btn-anim1 {
+  0% {
+    left: -100%;
+  }
+  50%,100% {
+    left: 100%;
+  }
+}
+
+.login-box a span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #AB4DF7);
+  animation: btn-anim2 1s linear infinite;
+  animation-delay: .25s
+}
+
+@keyframes btn-anim2 {
+  0% {
+    top: -100%;
+  }
+  50%,100% {
+    top: 100%;
+  }
+}
+
+.login-box a span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(270deg, transparent, #AB4DF7);
+  animation: btn-anim3 1s linear infinite;
+  animation-delay: .5s
+}
+
+@keyframes btn-anim3 {
+  0% {
+    right: -100%;
+  }
+  50%,100% {
+    right: 100%;
+  }
+}
+
+.login-box a span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #AB4DF7);
+  animation: btn-anim4 1s linear infinite;
+  animation-delay: .75s
+}
+
+@keyframes btn-anim4 {
+  0% {
+    bottom: -100%;
+  }
+  50%,100% {
+    bottom: 100%;
+  }
+}
+
+
+
+
+.formTitle{
+  padding: 20px;
+  color: #591a92;
+  display: flex;
+  justify-content: center;
+}
 
 
 /* contact-form */
